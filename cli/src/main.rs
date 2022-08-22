afc1c7991b41f7285c16938721687610c16fa59d
Nicolas Burtey <nicolas.burtey@gmail.com>
2022-08-22 17:20:18 +0100

    initial commit

---
+++ cli/src/main.rs
@@ -0,0 +1,57 @@
+use anyhow::Result;
+use clap::{Parser, Subcommand};
+use log::{self, info, LevelFilter};
+use url::Url;
+
+#[derive(Parser, Debug)]
+#[clap(author, version, about, long_about = None)]
+struct Cli {
+    #[clap(short, long, value_parser)]
+    api: Option<String>,
+
+    #[clap(short, long, value_parser, default_value_t = false)]
+    debug: bool,
+
+    #[clap(subcommand)]
+    command: Commands,
+}
+
+#[derive(Subcommand, Debug)]
+enum Commands {
+    /// Get globals value from the instance
+    Getinfo {
+        #[clap(value_parser)]
+        name: Option<String>,
+    },
+}
+
+use galoy_client::globals::globals;
+
+fn main() {
+    log::set_max_level(LevelFilter::Warn);
+
+    let cli = Cli::parse();
+    if cli.debug {
+        log::set_max_level(LevelFilter::Debug);
+    }
+
+    let api_env = std::env::var("SERVER").unwrap_or(String::from("http://localhost:4002/graphql"));
+
+    let api = match cli.api {
+        None => api_env,
+        Some(value) => value,
+    };
+
+    Url::parse(&api).expect(&format!("Api url \"{api}\" is not valid"));
+
+    info!("using api: {api}");
+
+    match cli.command {
+        Commands::Getinfo { name } => {
+            let result = globals(&api).unwrap();
+            let serialized_str = serde_json::to_string(&result).unwrap();
+            println!("{}", serialized_str);
+        }
+        _ => panic!("need a command"),
+    };
+}
---
+++ galoy-client/src/globals.rs
@@ -0,0 +1,32 @@
+use anyhow::*;
+use graphql_client::{reqwest::post_graphql_blocking as post_graphql, GraphQLQuery};
+use reqwest::blocking::Client;
+// use serde::Serialize;
+
+use self::query_globals::QueryGlobalsGlobals;
+
+#[derive(GraphQLQuery)]
+#[graphql(
+    schema_path = "src/graphql/schema.graphql",
+    query_path = "src/graphql/query_globals.graphql",
+    response_derives = "Debug, Serialize"
+)]
+struct QueryGlobals;
+
+pub fn globals(api_url: &String) -> Result<QueryGlobalsGlobals, anyhow::Error> {
+    let client = Client::builder().build().expect("error creating client");
+
+    let variables = query_globals::Variables;
+
+    let response_body = post_graphql::<QueryGlobals, _>(&client, api_url, variables)
+        .expect("issue fetching response");
+
+    let response_data = response_body.data.expect("bad response from server");
+
+    let result = match response_data.globals {
+        Some(value) => value,
+        None => panic!("empty response"),
+    };
+
+    Ok(result)
+}
