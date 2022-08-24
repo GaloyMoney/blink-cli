1ce87ca0afdbb639c9c956d08d618becef73205a
Nicolas Burtey <nicolas.burtey@gmail.com>
2022-08-24 15:17:48 +0200

    chore: reorg file to map galoy-client
    adding intraledger_send query

--- src/client/queries.rs
+++ src/client/queries.rs
@@ -1,23 +1,33 @@
 use graphql_client::GraphQLQuery;
 
-pub use self::query_default_wallet::QueryDefaultWalletAccountDefaultWallet;
-
 type Username = String;
+type WalletId = String;
+type SatAmount = u64;
+type Memo = String;
 
 #[derive(GraphQLQuery)]
 #[graphql(
     schema_path = "src/client/graphql/schema.graphql",
-    query_path = "src/client/graphql/default_wallet.graphql",
+    query_path = "src/client/graphql/queries/default_wallet.graphql",
     response_derives = "Debug, Serialize"
 )]
 pub(super) struct QueryDefaultWallet;
+pub use self::query_default_wallet::QueryDefaultWalletAccountDefaultWallet;
 
+#[derive(GraphQLQuery)]
+#[graphql(
+    schema_path = "src/client/graphql/schema.graphql",
+    query_path = "src/client/graphql/queries/query_globals.graphql",
+    response_derives = "Debug, Serialize"
+)]
+pub(super) struct QueryGlobals;
 pub use self::query_globals::QueryGlobalsGlobals;
 
 #[derive(GraphQLQuery)]
 #[graphql(
     schema_path = "src/client/graphql/schema.graphql",
-    query_path = "src/client/graphql/query_globals.graphql",
+    query_path = "src/client/graphql/mutations/intraledger_send.graphql",
     response_derives = "Debug, Serialize"
 )]
-pub(super) struct QueryGlobals;
+pub(super) struct IntraLedgerPaymentSend;
+// pub use self::query_globals::QueryGlobalsGlobals;
