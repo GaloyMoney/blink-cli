0d4fdcb148d9de37675b0338a78d0b0f83717b8f
Eshan Vaid <eshanvaid01@gmail.com>
2023-06-22 23:03:54 +0530

    feat: create error enums

---
+++ src/app/errors.rs
@@ -0,0 +1,21 @@
+use std::path::PathBuf;
+
+use thiserror::Error;
+
+#[derive(Error, Debug)]
+pub enum TokenError {
+    #[error("Token file does not exist")]
+    TokenFileNotFound,
+    #[error("Failed to get home directory")]
+    HomeDirectoryNotFound,
+    #[error("Failed to create directory: {0}")]
+    FailedToCreateDirectory(PathBuf),
+    #[error("Failed to create token file: {0}")]
+    FailedToCreateFile(PathBuf),
+    #[error("Failed to write token to file: {0}")]
+    FailedToWriteToken(PathBuf),
+    #[error("Failed to read token: {0}")]
+    FailedToReadToken(PathBuf),
+    #[error("Failed to delete token file: {0}")]
+    FailedToDeleteFile(PathBuf),
+}
--- src/app/token.rs
+++ src/app/token.rs
@@ -1,13 +1,14 @@
-use anyhow::Context;
 use std::fs::{self, File};
 use std::io::Write;
 use std::path::PathBuf;
 
+use crate::app::errors::TokenError;
+
 pub const TOKEN_FILE_NAME: &str = "GALOY_TOKEN";
 pub const TOKEN_FOLDER_NAME: &str = ".galoy-cli";
 
-pub fn get_token_file_path() -> Result<PathBuf, anyhow::Error> {
-    let home_dir = dirs::home_dir().context("failed to get home directory")?;
+pub fn get_token_file_path() -> Result<PathBuf, TokenError> {
+    let home_dir = dirs::home_dir().ok_or(TokenError::HomeDirectoryNotFound)?;
     let token_dir = home_dir.join(TOKEN_FOLDER_NAME);
     Ok(token_dir.join(TOKEN_FILE_NAME))
 }
@@ -16,37 +17,36 @@ pub fn get_token() -> anyhow::Result<Option<String>> {
     let token_file = get_token_file_path()?;
 
     if token_file.exists() {
-        let token = fs::read_to_string(&token_file).with_context(|| {
-            format!("failed to read token from file '{}'", token_file.display())
-        })?;
+        let token = fs::read_to_string(&token_file)
+            .map_err(|_| TokenError::FailedToReadToken(token_file.clone()))?;
         Ok(Some(token))
     } else {
         Ok(None)
     }
 }
 
-pub fn save_token(token: &str) -> Result<(), anyhow::Error> {
+pub fn save_token(token: &str) -> Result<(), TokenError> {
     let token_file = get_token_file_path()?;
     let parent_dir = token_file.parent().unwrap().to_owned();
     fs::create_dir_all(&parent_dir)
-        .with_context(|| format!("failed to create directory '{}'", parent_dir.display()))?;
+        .map_err(|_| TokenError::FailedToCreateDirectory(parent_dir.clone()))?;
 
     let mut file = File::create(&token_file)
-        .with_context(|| format!("failed to create file '{}'", token_file.display()))?;
+        .map_err(|_| TokenError::FailedToCreateFile(token_file.clone()))?;
 
     file.write_all(token.as_bytes())
-        .with_context(|| format!("failed to write to file '{}'", token_file.display()))?;
+        .map_err(|_| TokenError::FailedToWriteToken(token_file.clone()))?;
 
     Ok(())
 }
 
-pub fn remove_token() -> Result<(), anyhow::Error> {
+pub fn remove_token() -> Result<(), TokenError> {
     let token_file = get_token_file_path()?;
     if token_file.exists() {
         fs::remove_file(&token_file)
-            .with_context(|| format!("failed to delete token file '{}'", token_file.display()))?;
+            .map_err(|_| TokenError::FailedToDeleteFile(token_file.clone()))?;
         Ok(())
     } else {
-        Err(anyhow::anyhow!("Token file does not exist"))
+        Err(TokenError::TokenFileNotFound)
     }
 }
