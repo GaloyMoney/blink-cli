7960ad631ea6cdb8a9db711e610e5eba69edbcf4
Eshan Vaid <eshanvaid01@gmail.com>
2023-06-29 21:19:02 +0530

    fix: remove SetUsernameError

--- src/errors/mod.rs
+++ src/errors/mod.rs
@@ -1,14 +1,12 @@
 use thiserror::Error;
 
 use self::{
-    api_error::ApiError, me_error::MeError, payment_error::PaymentError,
-    set_username_error::SetUsernameError, token_error::TokenError,
+    api_error::ApiError, me_error::MeError, payment_error::PaymentError, token_error::TokenError,
 };
 
 pub mod api_error;
 pub mod me_error;
 pub mod payment_error;
-pub mod set_username_error;
 pub mod token_error;
 
 #[derive(Error, Debug)]
@@ -19,8 +17,6 @@ pub enum CliError {
     ApiError(#[from] ApiError),
     #[error(transparent)]
     TokenError(#[from] TokenError),
-    #[error(transparent)]
-    SetUsernameError(#[from] SetUsernameError),
     #[error(transparent)]
     PaymentError(#[from] PaymentError),
 }
