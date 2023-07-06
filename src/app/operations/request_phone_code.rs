8fbdb9339653d27d54585a1eee923ce28a419898
Eshan <eshanvaid01@gmail.com>
2023-07-07 00:15:48 +0530

    feat: auto open browser

--- src/app/operations/request_phone_code.rs
+++ src/app/operations/request_phone_code.rs
@@ -1,15 +1,18 @@
 use crate::app::{server::server::run, App};
 use anyhow::Result;
 use std::net::TcpListener;
+use webbrowser;
 
 impl App {
     pub async fn request_phone_code(&self, phone: String) -> Result<()> {
         let listener = TcpListener::bind("127.0.0.1:0")?;
         let port = listener.local_addr().unwrap().port();
-        println!(
-            "Visit http://127.0.0.1:{}/login and solve the Captcha",
-            port
-        );
+        let url = format!("http://127.0.0.1:{}/login", port);
+
+        webbrowser::open(&url)?;
+
+        println!("Visit {} and solve the Captcha", url);
+
         let api_clone = self.client.api.clone();
         let captcha_challenge = self.client.create_captcha_challenge().await?;
         tokio::task::spawn_blocking(move || {
