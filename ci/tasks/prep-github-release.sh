7970585962b4a291fbab01e2e5994993868ce0fc
Sandipan Dey <hey@sandipan.dev>
2023-08-01 14:20:16 +0530

    ci: save sha256sums

--- ci/tasks/prep-github-release.sh
+++ ci/tasks/prep-github-release.sh
@@ -8,9 +8,5 @@ mv x86_64-pc-windows-gnu/* artifacts/binaries
 mv x86_64-apple-darwin/* artifacts/binaries
 mv x86_64-unknown-linux-musl/* artifacts/binaries
 
-for file in artifacts/binaries/*; do
-    if [ -f "$file" ]; then
-        sha256sum "$file" > "$file".sha256
-    fi
-done
-
+cd artifacts/binaries
+sha256sum * > sha256sums.txt
